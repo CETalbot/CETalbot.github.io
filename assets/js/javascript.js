5833c8091a4caf0b55d4dcdae02401b81dd9a39d
Catherine <ctalbot5@uclan.ac.uk>
2018-01-10 22:09:22 +0000

    adding comments to code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,14 @@
+	//carousel background code referenced to http://kenwheeler.github.io/slick/
 	$(document).ready(function(){
 	  $(".your-class").slick({
 		  lazyLoad: "ondemand",
+		  //shows three images when page is loaded 
 		  slidesToShow: 3,
+		  //when user scrolls it only scrolls to one image at a time 
 		  slidesToScroll: 1,
-		  //prevArrow: $(".previous"),
-		  //nextArrow: $(".next")
+		  //displays the arrows within the slick library
 		  arrows:true,
+		  //centers slick carousel
 		  centerMode:true
 	  });
 	});
